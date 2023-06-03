ab228804916ba0c2c9fef638b12fab1db8a0fb51
unknown <ahmadshah3236@gmail.com>
2023-06-03 15:42:34 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Resturant from './component/Basics/Resturant'
+const App = () => {
+  return (
+    <>
+      <h2 className='dhaba-name'>Dhaba</h2>
+      <Resturant />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,87 @@
+const Menu = [
+    {
+        id: 1,
+        image: "images/bread.jpeg",
+        name: "Bread",
+        category: "breakfast",
+        price: "120",
+        description:
+            "For a delightful breakfast, there's nothing quite like a slice of freshly baked bread. Whether it's a crusty baguette, a fluffy brioche, or a hearty whole wheat loaf, bread is a versatile and comforting staple that can elevate your morning meal . ",
+    },
+
+    {
+        id: 2,
+        image: "images/allupakoida.jpeg",
+        name: "pakory",
+        category: "evening",
+        price: "200",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 3,
+        image: "images/egg.jpeg",
+        name: "egg",
+        category: "breakfast",
+        price: "40",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 4,
+        image: "../images/puri.jpeg",
+        name: "Halva Puri",
+        category: "breakfast",
+        price: "150",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 5,
+        image: "../images/pizza.jpeg",
+        name: "pizza",
+        category: "evening",
+        price: "1000",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 6,
+        image: "../images/platter.jpeg",
+        name: "Non-veg Platter",
+        category: "dinner",
+        price: "800",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+
+    {
+        id: 7,
+        image: "../images/sweet.jpeg",
+        name: "Sweets",
+        category: "dinner",
+        price: "100",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 8,
+        image: "../images/biryani.jpeg",
+        name: "biryani",
+        category: "lunch",
+        price: "200",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 9,
+        image: "../images/samosa.jpeg",
+        name: "samaso",
+        category: "evening",
+        price: "60",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+];
+
+export default Menu;
